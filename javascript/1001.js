094b19cdff0fb3be330a78cdf641716273789188
Regy Eduardo <cositas@aol.com>
2020-08-12 01:16:59 -0300

    1001 JS

---
+++ javascript/1001.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('', (a) => {
+    rl.question('', (b) => {
+        var x = Number(a) + Number(b);
+        console.log(`X = ${x}`);
+        rl.close();
+    });
+});
+
+// const a = lines();
+// const b = lines();
+// const x = Number(a) + Number(b)
+// console.log(`X = ${x}`);
